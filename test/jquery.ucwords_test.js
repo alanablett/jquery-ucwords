b61ef8cd37c9035858ca014c97934fd42e50487b
Alan Ablett <alan.ablett1@googlemail.com>
2014-08-13 13:50:08 +0300

    Added a dummy error

--- test/jquery.ucwords_test.js
+++ test/jquery.ucwords_test.js
@@ -33,4 +33,8 @@
 		strictEqual( this.fixture.ucwords(3, 2000).text(), 'Lorem ipsum dolor Sit Amet Consectetur Adipisicing.' );
 	});
 	
+	test('introduce a false error for travis', 1, function(){
+		strictEqual( false, true );
+	});
+	
 })(jQuery);
